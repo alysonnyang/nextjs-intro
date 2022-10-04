0fb6d42f99a3c9badac5637be735f85753438569
alysonnyang <alyson.n.yang@gmail.com>
2022-10-04 11:12:51 +0900

    Create [id].js

---
+++ pages/movies/[id].js
@@ -0,0 +1,13 @@
+import { useRouter } from "next/router";
+
+export default function Detail() {
+  const router = useRouter();
+  return (
+    <div>
+      <h4>{router.query.title || "Loading..."}</h4>
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${router.query.poster_path}`}
+      />
+    </div>
+  );
+}
