7cd133dded256c5e17c4375929485ef1b88581ce
alysonnyang <alyson.n.yang@gmail.com>
2022-10-04 11:11:02 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const API_KEY = "10923b261ba94d897ac6b81148314a3f";
+
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/movies/:id",
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
